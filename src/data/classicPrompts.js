850a86f10050ebba73d7118549dcb82e10e1127a
Janko <janko.prester@gmail.com>
2021-06-09 15:40:23 +0200

    remove extra prompt

--- src/data/classicPrompts.js
+++ src/data/classicPrompts.js
@@ -33,11 +33,6 @@ const ClassicPrompts = [
     id: 6,
     text: "John F. Kennedy was just elected President of the United States after rising from the grave decades after his assassination. Due to miraculous developments in nanotechnology, Kennedy’s brain was rebuilt from his remains and installed in the control center of a state-of-the art humanoid robot. Below is a transcript of his acceptance speech.",
     source_link: "https://openai.com/blog/better-language-models/"
-  },
-  {
-    id: 7,
-    text: "John F. Kennedy was just elected President of the United States after rising from the grave decades after his assassination. Due to miraculous developments in nanotechnology, Kennedy’s brain was rebuilt from his remains and installed in the control center of a state-of-the art humanoid robot. Below is a transcript of his acceptance speech.",
-    source_link: "https://openai.com/blog/better-language-models/"
   }
 ]
 
