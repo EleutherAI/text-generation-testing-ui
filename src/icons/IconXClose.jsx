25da6056cfdfbb009e4c9cf9f9767ecb09228931
Janko <janko.prester@gmail.com>
2021-09-08 22:54:49 +0200

    fix for background colors and svgs

--- src/icons/IconXClose.jsx
+++ src/icons/IconXClose.jsx
@@ -9,12 +9,10 @@ export default function IconXClose(props) {
       aria-hidden="true"
       focusable="false"
       role="img"
-      class="iconify iconify--clarity"
       preserveAspectRatio="xMidYMid meet"
       viewBox="0 0 36 36"
       {...props}>
       <path
-        class="clr-i-outline clr-i-outline-path-1"
         d="M19.41 18l8.29-8.29a1 1 0 0 0-1.41-1.41L18 16.59l-8.29-8.3a1 1 0 0 0-1.42 1.42l8.3 8.29l-8.3 8.29A1 1 0 1 0 9.7 27.7l8.3-8.29l8.29 8.29a1 1 0 0 0 1.41-1.41z"
         fill={fill || "#34566f"}></path>
     </svg>
