56189d64c52e9447e88b38ee441370876dfc4c3a
Janko <janko.prester@gmail.com>
2021-09-08 18:53:01 +0200

    refactor ui to components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="content-wrapper">
+        <div className="footer-content">
+          <div className="footer-text-center">EleutherAI 2021</div>
+        </div>
+      </div>
+    </footer>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Header() {
+  return (
+    <header className="header-container">
+      <div className="content-wrapper row">
+        <a href="https://www.eleuther.ai/">
+          <img id="logo" className="logo-image" src="img/eai_logo_small_exported.png" alt="EleutherAI logo" />
+        </a>
+        <h1 className="logo-text">
+          <a href="/">EleutherAI</a>
+        </h1>
+      </div>
+    </header>
+  )
+}
