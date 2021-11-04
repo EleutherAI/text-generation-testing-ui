fea1f638b0b485e9f19961a36ce5455631ab3c87
saharhash <saharhashai@gmail.com>
2021-11-04 13:05:47 -0700

    Select dropdown dark mode fixes

--- src/components/PlaygroundHeader.jsx
+++ src/components/PlaygroundHeader.jsx
@@ -5,6 +5,39 @@ import "../styles/playgroundHeader.scss"
 import PresetPrompts from "../data/presetPrompts"
 
 export default function PlaygroundHeader({ isDarkMode, presetSelected }) {
+  // taken from colors.scss
+  const DARK_THEME_BG2 = "#1d2327"
+  const DARK_THEME_TEXT = "#dbdbdb"
+  const DARK_THEME_BLUE = "#00bbff"
+
+  const customStyles = isDarkMode => ({
+    control: (provided, state) => ({
+      ...provided,
+      backgroundColor: isDarkMode ? DARK_THEME_BG2 : "white"
+    }),
+    menu: (provided, state) => ({
+      ...provided,
+      backgroundColor: isDarkMode ? DARK_THEME_BG2 : "white"
+    }),
+    singleValue: (provided, state) => ({
+      ...provided,
+      color: isDarkMode ? DARK_THEME_TEXT : "black"
+    }),
+    dropdownIndicator: (provided, state) => ({
+      ...provided,
+      color: isDarkMode ? "white" : DARK_THEME_TEXT,
+      "&:hover": {
+        color: isDarkMode ? DARK_THEME_TEXT : "black"
+      }
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      "&:hover": {
+        backgroundColor: isDarkMode ? DARK_THEME_BLUE : provided
+      }
+    })
+  })
+
   return (
     <div className="content-wrapper playground-header">
       <div className="title">
@@ -15,6 +48,7 @@ export default function PlaygroundHeader({ isDarkMode, presetSelected }) {
 
       {process.env.REACT_APP_PROMPT_SUGGESTIONS === "true" &&
       <Select className="preset-section" options={PresetPrompts} placeholder="Try a preset prompt..."
+              menuColor="green" styles={customStyles(isDarkMode)}
               onChange={presetSelected} />
       }
     </div>
--- src/components/PromptInput.jsx
+++ src/components/PromptInput.jsx
@@ -5,7 +5,7 @@ import "../styles/promptInput.scss"
 export default function PromptInput({ setPromptText, promptText, onKeyDown }) {
   return (
     <div className="prompt-input">
-      <textarea className="prompt-textarea" placeholder="Write some text and run to generate text." value={promptText}
+      <textarea className="prompt-textarea" placeholder="Write some text and run (⇧+Enter) to generate text." value={promptText}
                 onKeyDown={onKeyDown}
                 onChange={evt => {
                   setPromptText(evt.currentTarget.value)
